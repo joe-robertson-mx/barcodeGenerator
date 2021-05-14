d7f84ee53925ee2748daf10cefae63255d497ad9
Joe Robertson <32719006+joerob319@users.noreply.github.com>
2021-05-14 18:32:21 +0100

    Readme update

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 ## BarcodeGenerator
-[Generate barcodes from strings]
+A Mendix Pluggable Widget to generate barcodes in different formats from string attributes.
+Utilises the react-barcode library https://github.com/kciter/react-barcode
 
 ## Features
-[feature highlights]
+Supported barcodes:
+    - CODE128
+    - EAN-13
+    - EAN-8
+    - EAN-5
+    - EAN-2
+    - UPC
+    - ITF14
+
+Width and Height can be set from the widget.
 
 ## Usage
-[step by step instructions]
-
-## Demo project
-[link to sandbox]
+The Pluggable Widget can be downloaded from the Mendix Marketplace (https://marketplace.mendix.com/) and installed in a Mendix application.
 
 ## Issues, suggestions and feature requests
-[link to GitHub issues]
+https://github.com/joerob319/barcodeGenerator/issues
 
 ## Development and contribution
-[specify contribute]
+https://github.com/joerob319/barcodeGenerator/pulls
--- src/BarcodeGenerator.editorPreview.tsx
+++ src/BarcodeGenerator.editorPreview.tsx
@@ -1,13 +1,13 @@
-// import { ReactNode, createElement } from "react";
-// import { BarcodeComponent } from "./components/BarcodeComponent";
-// import { BarcodeGeneratorPreviewProps } from "../typings/BarcodeGeneratorProps";
+import { ReactNode, createElement } from "react";
+import { BarcodeComponent } from "./components/BarcodeComponent";
+import { BarcodeGeneratorPreviewProps } from "../typings/BarcodeGeneratorProps";
 
-// declare function require(name: string): string;
+declare function require(name: string): string;
 
-// export const BarcodeGenerator = (props: BarcodeGeneratorPreviewProps): ReactNode => {
-//     return <BarcodeComponent inputString={props.value} />;
-// };
+export const BarcodeGenerator = (props: BarcodeGeneratorPreviewProps): ReactNode => {
+    return <BarcodeComponent inputString={props.value} />;
+};
 
-// export function getPreviewCss(): string {
-//     return require("./ui/BarcodeGenerator.css");
-// }
+export function getPreviewCss(): string {
+    return require("./ui/BarcodeGenerator.css");
+}
